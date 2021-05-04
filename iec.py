00e8fe347b2a2bdf305d22e7b3e894baea52ea1b
Steven Walter <stevenrwalter@gmail.com>
2021-05-03 22:52:30 -0400

    GOT A LISTEN 8git diff

--- iec.py
+++ iec.py
@@ -7,6 +7,7 @@ SENDER_READY = 1
 RX_READY = 2
 ACTIVE = 3
 WAIT_FOR_RX_ACK = 4
+EOI_ACK = 5
 
 active_count = 0
 buffer = 0
@@ -15,10 +16,13 @@ state = IDLE
 clk = 1
 data = 0
 atn = 0
+eoi_counter = 0
+byte_counter = 0
+last_byte = False
 
 def recv_byte():
     print("Got byte {:02x}".format(buffer))
-    if atn == 1:
+    if atn == 1 and byte_counter == 0:
         if buffer == 0x3f:
             print("UNLISTEN")
         elif buffer == 0x5f:
@@ -42,12 +46,16 @@ for l in sys.stdin.readlines():
         continue
 
     if "DD00 store" in l:
+        #print(l)
         if "ATN" in l:
             if atn == 0:
+                print("ATTN")
                 state = SENDER_READY
             atn = 1
         else:
             if atn == 1:
+                byte_counter = 0
+                print("END OF ATTENTION")
                 state = IDLE
             atn = 0
 
@@ -58,26 +66,27 @@ for l in sys.stdin.readlines():
         last_data = data
 
         # VICE logging already inverts the DATA/CLK inputs
-        if "CLK" in l:
+        if "CLK IN" in l:
             clk = 1
         else:
             clk = 0
 
-        if "DATA" in l:
+        if "DATA IN" in l:
             data = 1
         else:
-            data = 0;
+            data = 0
 
         if last_clk == 0 and clk == 1:
             if state == ACTIVE:
                 active_count += 1
                 if active_count == 8:
                     recv_byte()
+                    byte_counter += 1
                     state = WAIT_FOR_RX_ACK
                     active_count = 0
                     buffer = 0
                 else:
-                    buffer <<= 1
+                    buffer >>= 1
 
             if state == RX_READY and data == 0:
                 state = ACTIVE
@@ -87,14 +96,26 @@ for l in sys.stdin.readlines():
             if state == IDLE:
                 state = SENDER_READY
             elif state == ACTIVE:
-                buffer |= data
+                if data == 0:
+                    buffer |= 1 << 7
 
         if last_data == 1 and data == 0:
             if state == SENDER_READY and clk == 0:
                 state = RX_READY
         elif last_data == 0 and data == 1:
             if state == WAIT_FOR_RX_ACK:
+                if last_byte:
+                    byte_counter = 0
                 state = IDLE
+            elif state == EOI_ACK:
+                state = ACTIVE
+
+        if state == RX_READY and data == 0 and clk == 0:
+            eoi_counter += 1
+            if eoi_counter >= 30:
+                print("LAST BYTE")
+                last_byte = True
+                state = EOI_ACK
 
     #print("atn {} state {} active_count {} buffer {:02x}".format(atn, state, active_count, buffer))
     #print("clk {} data {} atn {} state {} active_count {}".format(clk, data, atn, state, active_count))
